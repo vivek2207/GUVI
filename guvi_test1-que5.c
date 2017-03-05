8cafde49020a90a0e9e46d5596b143a8f284e2b8
vivek2207 <vivek2207@users.noreply.github.com>
2017-03-05 16:39:58 +0530

    Add files via upload

---
+++ guvi_test1-que5.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+	
+	int a, b, c;
+	scanf("%d%d%d", &a, &b, &c);
+	
+	
+	if(a>=b && a>c)
+		printf("%d is largest number", a);
+	else if(b>=a && b>c)
+		printf("%d is largest number", b);
+	else if(c>=a && c>b)
+		printf("%d is largest number", c);	
+}
